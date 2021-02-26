915c56357cde3769ebef448006ee591d6356f950
Omar Branez <omar.branez@gmail.com>
2021-02-26 16:26:04 -0500

    commit tree 349f5f8098e61e75cc5d35e0213b9fe82757c2e4

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,22 @@
+class Genre
+    attr_accessor :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select{|songs| songs.genre == self}
+    end
+
+    def artists
+        songs.map{|song| song.artist}
+    end
+
+end
